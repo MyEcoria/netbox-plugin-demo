b7fc9f3b3c2c84bc9fa7ab024d0b0fb48c29f72c
Pierric Buchez <contact@myecoria.com>
2025-07-20 22:15:21 +0200

    [~]: Update navigation links in PluginMenuItem to reflect plugin renaming

--- counter_plugin/navigation.py
+++ counter_plugin/navigation.py
@@ -1,10 +1,8 @@
 from extras.plugins import PluginMenuItem
 
-from extras.plugins import PluginMenuItem
-
 menu = (
     PluginMenuItem(
-        link='plugins:netbox_access_lists:counter',
-        link_text='Access Lists',
+        link='plugins:counter_plugin:counter',
+        link_text='Access Lists Counter',
     ),
 )
