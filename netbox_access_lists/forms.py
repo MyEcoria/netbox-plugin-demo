b7cbfc984524c2d7aadc2c1a709533c53f094120
Pierric Buchez <contact@myecoria.com>
2025-07-20 18:04:55 +0200

    [~]: Refactor forms for AccessList and AccessListRule with updated Meta configurations

--- netbox_access_lists/forms.py
+++ netbox_access_lists/forms.py
@@ -1,29 +1,31 @@
-from django.urls import path
+from ipam.models import Prefix
+from netbox.forms import NetBoxModelForm
+from utilities.forms.fields import CommentField, DynamicModelChoiceField
+from .models import AccessList, AccessListRule
 
-from netbox.views.generic import ObjectChangeLogView
-from . import models, views
+
+class AccessListForm(NetBoxModelForm):
+    comments = CommentField()
+
+    class Meta:
+        model = AccessList
+        fields = ('name', 'default_action', 'comments', 'tags')
 
 
-urlpatterns = (
+class AccessListRuleForm(NetBoxModelForm):
+    access_list = DynamicModelChoiceField(
+        queryset=AccessList.objects.all()
+    )
+    source_prefix = DynamicModelChoiceField(
+        queryset=Prefix.objects.all()
+    )
+    destination_prefix = DynamicModelChoiceField(
+        queryset=Prefix.objects.all()
+    )
 
-    # Access lists
-    path('access-lists/', views.AccessListListView.as_view(), name='accesslist_list'),
-    path('access-lists/add/', views.AccessListEditView.as_view(), name='accesslist_add'),
-    path('access-lists/<int:pk>/', views.AccessListView.as_view(), name='accesslist'),
-    path('access-lists/<int:pk>/edit/', views.AccessListEditView.as_view(), name='accesslist_edit'),
-    path('access-lists/<int:pk>/delete/', views.AccessListDeleteView.as_view(), name='accesslist_delete'),
-    path('access-lists/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='accesslist_changelog', kwargs={
-        'model': models.AccessList
-    }),
-
-    # Access list rules
-    path('rules/', views.AccessListRuleListView.as_view(), name='accesslistrule_list'),
-    path('rules/add/', views.AccessListRuleEditView.as_view(), name='accesslistrule_add'),
-    path('rules/<int:pk>/', views.AccessListRuleView.as_view(), name='accesslistrule'),
-    path('rules/<int:pk>/edit/', views.AccessListRuleEditView.as_view(), name='accesslistrule_edit'),
-    path('rules/<int:pk>/delete/', views.AccessListRuleDeleteView.as_view(), name='accesslistrule_delete'),
-    path('rules/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='accesslistrule_changelog', kwargs={
-        'model': models.AccessListRule
-    }),
-
-)
+    class Meta:
+        model = AccessListRule
+        fields = (
+            'access_list', 'index', 'description', 'source_prefix', 'source_ports', 'destination_prefix',
+            'destination_ports', 'protocol', 'action', 'tags',
+        )
