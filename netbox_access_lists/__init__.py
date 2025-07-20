fe945f3b9bd626637c8bf361c438254d21b16ccd
Pierric Buchez <contact@myecoria.com>
2025-07-20 21:17:53 +0200

    [~]: Update plugin name and base URL to 'counter-plugin'

--- netbox_access_lists/__init__.py
+++ netbox_access_lists/__init__.py
@@ -1,13 +1,13 @@
 from netbox.plugins import PluginConfig
 
 class NetBoxAccessListsConfig(PluginConfig):
-    name = 'counter_plugin'
+    name = 'counter-plugin'
     verbose_name = 'Access Lists'
     description = 'A simple plugin to demonstrate a basic counter.'
     version = '0.2'
     author = 'Your Name'
     author_email = 'your.email@example.com'
-    base_url = 'access-lists'
+    base_url = 'counter-plugin'
     required_settings = []
     default_settings = {}
 
